0b0547fa3ae1172e85ba5dc7e59866ab2f582f9c
Mikael Messias <mikaelmessias@outlook.com>
2021-03-14 12:15:39 -0300

    refactor: rename jsx files to tsx and types
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,21 +3,20 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
-const repository = {
-  name: "unform",
-  description: "Forms in React",
-  link: "https://github.com/unform/unform",
-};
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
 
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/mikaelmessias-lab/repos')
       .then(response => response.json())
       .then(data => setRepositories(data));
     }, []);
-
 
   return (
     <section className="repository-list">
